525d7f84ec7290e15e7f4c4d8bc982fba74f2857
SAIEED12 <abusaieed0@gmail.com>
2026-03-17 08:09:19 +0600

    Navbar

--- src/Components/DaisyNav/NavBar/Link.jsx
+++ src/Components/DaisyNav/NavBar/Link.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Link = ({route}) => {
     return (
         <li className='mr-10'>
-            <a href={route.ptah}>{route.name}</a>
+            <a href={route.path}>{route.name}</a>
         </li>
     );
 };
--- src/Components/DaisyNav/NavBar/NavBar.jsx
+++ src/Components/DaisyNav/NavBar/NavBar.jsx
@@ -1,57 +1,71 @@
-import React from 'react';
-import Link from './Link';
+import React, { useState } from "react";
+import Link from "./Link";
+import { Menu, X } from "lucide-react";
 
 const navigationData = [
   {
     id: 1,
     name: "Home",
-    path: "/"
+    path: "/",
   },
   {
     id: 2,
     name: "About",
-    path: "/about"
+    path: "/about",
   },
   {
     id: 3,
     name: "Services",
-    path: "/services"
+    path: "/services",
   },
   {
     id: 4,
     name: "Blog",
-    path: "/blog"
+    path: "/blog",
   },
   {
     id: 5,
     name: "Contact",
-    path: "/contact"
-  }
+    path: "/contact",
+  },
 ];
 
-
 const NavBar = () => {
-    return (
-        <nav>
-            <ul className='flex'>
-                {
-                    navigationData.map(route => <Link key={route.id} route={route}></Link>)
-                }
-            </ul>
-            {/* <ul className='flex'>
+
+    const [open, setOpen] = useState(false)
+    const links = navigationData.map((route) => (<Link key={route.id} route={route}></Link>))
+
+  return (
+    <nav className="flex justify-between mx-10">
+      <span className="flex" onClick={() => setOpen(!open)}>
+        {open ? <X className="md:hidden"></X> : <Menu className="md:hidden"></Menu>}
+
+        <ul className="md:hidden">
+            {links}
+        </ul>
+
+        <h3 className="ml-4">My Navbar</h3>
+      </span>
+      <ul className="md:flex hidden">
+        {
+            links
+        }
+      </ul>
+      {/* <ul className='flex'>
                 {
                     navigationData.map(route =>  <li className='mr-10'><a href={route.path}>{route.name}</a></li>)
                 }
             </ul> */}
 
-
-            {/* <ul className='flex'>
+      {/* <ul className='flex'>
                 <li className='mr-10'><a href="/">Home</a></li>
                 <li className='mr-10'><a href="/about">About</a></li>
                 <li className='mr-10'><a href="/blog">Blog</a></li>
             </ul> */}
-        </nav>
-    );
+
+      <button>Sign In</button>
+    </nav>
+  );
 };
 
 export default NavBar;
